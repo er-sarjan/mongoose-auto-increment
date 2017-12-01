6453c1fd393b0264c99a9592b4882dbed05182b6
Sarjan Desai <sarjan.desai@solutionanalysts.com>
2017-12-01 15:30:31 +0530

    Fix index issue for required :bug:
    - Change require to required in schema

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ exports.initialize = function (connection) {
     if (ex.name === 'MissingSchemaError') {
       // Create new counter schema.
       counterSchema = new mongoose.Schema({
-        model: { type: String, require: true },
-        field: { type: String, require: true },
+        model: { type: String, required: true },
+        field: { type: String, required: true },
         count: { type: Number, default: 0 }
       });
 
       // Create a unique index using the "field" and "model" fields.
-      counterSchema.index({ field: 1, model: 1 }, { unique: true, required: true, index: -1 });
+      counterSchema.index({ field: 1, model: 1 }, { unique: true });
 
       // Create model using new schema.
       IdentityCounter = connection.model('IdentityCounter', counterSchema);
